ccf053d1320c8c5e07b3b0bc59b86fbb7f8d175e
ThilinaMadhusanka <140851032+ThilinaMadhusanka@users.noreply.github.com>
2025-07-01 05:41:03 -0700

    Create navbar and header

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className="header-contents">
+                <h2>Order Your Favourit Food Here</h2>
+                <p>
+                    This is a long-form dummy description intended to simulate realistic content. It can be used to test UI layouts, text wrapping, or content preview functionality in apps, websites, or databases. Feel free to replace this with actual content later.
+                </p>
+                <button>View Menu</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./Home.css";
+import Header from '../../components/Header/Header';
+export const Home = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    )
+}
