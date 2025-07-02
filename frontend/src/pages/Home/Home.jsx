924b3b5c059c86a478cdac0a9536af07c9fc5aef
ThilinaMadhusanka <140851032+ThilinaMadhusanka@users.noreply.github.com>
2025-07-02 04:06:12 -0700

    Create explore our menu section

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import "./Home.css";
 import Header from '../../components/Header/Header';
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
+
+
 export const Home = () => {
     return (
         <div>
             <Header />
+            <ExploreMenu />
         </div>
     )
 }
